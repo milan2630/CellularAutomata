43ce93f68578f43898d2715c2ad1b1b7c6c8c7e2
CAYLA SCHUVAL <cas169@duke.edu>
2020-01-28 00:26:26 -0500

    started/ outlined  cell and board class

--- src/cellsociety/Board.java
+++ src/cellsociety/Board.java
@@ -1,4 +1,33 @@
 package cellsociety;
 
+
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+
 public class Board {
+    private Cell[][] myCells;
+    private int num_rows;
+    private int num_cols;
+    private int dist_between_bricks;
+    private Color cell_color;
+    private double yPos;
+    private double xPos;
+    private double height;
+
+    public Board(int num_Cells_width, int num_Cells_Height){
+        for(int i=0; i<num_Cells_Height; i++){
+            for (int j=0; j<num_Cells_width; j++){
+                Cell myCell = new Cell(state, cell_color, 0, 0);
+                myCells[i][j]= myCell;
+                yPos = yPos + myCell.getWidth();
+                height = myCell.getHeight();
+            }
+            yPos = 0;
+            xPos = xPos + height;
+        }
+    }
+    public Group boardView(){
+
+    }
+
 }
--- src/cellsociety/Cell.java
+++ src/cellsociety/Cell.java
@@ -1,4 +1,26 @@
 package cellsociety;
 
-public class Cell {
+import java.util.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Cell extends Rectangle{
+    private int myState;
+    private int turns_since_state_change;
+    private List<Cell> neighbors;
+    private Color state_color;
+
+
+    public Cell(int init_state, Color disp_color, int xPos, int yPos){
+        super();
+        myState=init_state;
+        state_color=disp_color;
+        setFill(disp_color);
+        setX(xPos);
+        setY(yPos);
+    }
+
+    public void changeStateAndView(int state){
+        myState= state;
+    }
 }
