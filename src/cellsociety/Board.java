f11f163e0807ee95f34e1158700ebefc717a1ad3
CAYLA SCHUVAL <cas169@duke.edu>
2020-01-29 20:36:49 -0500

    trying to push

--- src/cellsociety/Board.java
+++ src/cellsociety/Board.java
@@ -5,43 +5,67 @@ import javafx.scene.Group;
 import javafx.scene.paint.Color;
 
 public class Board {
-    private Cell[][] myCells;
-    private int num_rows;
-    private int num_cols;
-    private int dist_between_bricks;
-    private Color cell_color;
-    private double yPos;
-    private double xPos;
-    private double height;
-    private Group root;
 
-    public Board(int num_Cells_width, int num_Cells_Height){
-        for(int i=0; i<num_Cells_Height; i++){
-            for (int j=0; j<num_Cells_width; j++){
-                Cell myCell = new Cell(state, cell_color, 0, 0);
-                myCells[i][j]= myCell;
-                yPos = yPos + myCell.getWidth();
-                height = myCell.getHeight();
-            }
-            yPos = 0;
-            xPos = xPos + height;
-        }
+  private Cell[][] myCells;
+  private int num_rows;
+  private int num_cols;
+  private int dist_between_bricks;
+  private Color cell_color;
+  private double height;
+  private double width;
+  private Group root;
+  private double xPos = 0;
+  private double yPos = 0;
+
+  public Board(int num_Cells_width, int num_Cells_Height) {
+    for (int i = 0; i < num_Cells_Height; i++) {
+      for (int j = 0; j < num_Cells_width; j++) {
+        Cell myCell = new Cell(0, Color.BLUE);
+        myCells[i][j] = myCell;
+        height = myCell.getHeight();
+        width = myCell.getWidth();
+        setCellPositions(myCell, i, j);
+        updateYPos();
+      }
+      updateXPos();
     }
-    public Group boardView(){
-        root = new Group();
-        addCellsToRoot();
-        return root;
-        
+  }
+
+  private void setCellPositions(Cell cell, int col, int row) {
+    cell.setX(xPos);
+    cell.setY(yPos);
+  }
+
+  private void updateYPos() {
+    yPos = yPos + width;
+  }
+
+  private void updateXPos() {
+    xPos = xPos + height;
+  }
+
+
+  public void insertCell(Cell cell, int row, int col) {
+    myCells[col][row] = cell;
+  }
+
+
+  // did not write yet
+  public void updateBoard() {
+  }
+
+  public Group boardView() {
+    root = new Group();
+    addCellsToRoot();
+    return root;
+
+  }
+
+  private void addCellsToRoot() {
+    for (Cell[] cell_row : myCells) {
+      for (Cell cell : cell_row) {
+        root.getChildren().add(cell);
+      }
     }
-
-    private void addCellsToRoot(){
-        for(Cell[] cell_row : myCells){
-            for(Cell cell: cell_row){
-                root.getChildren().add(cell);
-
-            }
-        }
-
-    }
-
+  }
 }
--- src/cellsociety/Cell.java
+++ src/cellsociety/Cell.java
@@ -4,23 +4,41 @@ import java.util.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-public class Cell extends Rectangle{
-    private int myState;
-    private int turns_since_state_change;
-    private List<Cell> neighbors;
-    private Color state_color;
+public class Cell extends Rectangle {
+
+  private int myState;
+  private int turns_since_state_change;
+  private List<Cell> neighbors;
+  private Color myColor;
 
 
-    public Cell(int init_state, Color disp_color, int xPos, int yPos){
-        super();
-        myState=init_state;
-        state_color=disp_color;
-        setFill(disp_color);
-        setX(xPos);
-        setY(yPos);
+
+  public Cell(int init_state, Color disp_color) {
+    super();
+    myState = init_state;
+    myColor = disp_color;
+    setFill(disp_color);
+    //setX(xPos);
+    //setY(yPos);
+  }
+
+  public void changeStateAndView(int state, Color viewColor) {
+    if(state == myState) {
+      turns_since_state_change++;
     }
-
-    public void changeStateAndView(int state){
-        myState= state;
+    else {
+      turns_since_state_change=0;
+      myState = state;
+      myColor=viewColor;
     }
+  }
+
+  public int numberOfStateChanges(){
+    return turns_since_state_change;
+  }
+
+  public int getState(){
+    return myState;
+  }
+
 }
