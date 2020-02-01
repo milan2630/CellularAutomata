c9679cae35f75ae95e4eee59bd7eb4cf8fdbd8d3
Dana Mulligan <dana.mulligan@duke.edu>
2020-01-31 20:19:08 -0500

    removed constructor parameter

--- src/cellsociety/Visualizer.java
+++ src/cellsociety/Visualizer.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
@@ -16,23 +17,21 @@ public class Visualizer extends Application {
 
     public static final int CA_WIDTH = 400; //TODO THIS CAN BE CHANGED LATER
     public static final int CA_HEIGHT = 400;
-    public static final Color BACKGROUND = Color.PALETURQUOISE; //TODO THIS CAN BE CHANGED LATER
+    public static final Color BACKGROUND = Color.RED; //TODO THIS CAN BE CHANGED LATER
+    public static final double GAP = 8;
     private Group root;
     private Scene myScene;
-    private Stage myStage;
+    private GridPane grid;
 
-    private String intro = "Welcome to Dana's Game!";
-    private int introTextHeight = 20;
-    private String rules = "Some Rules:\n1. Press space to begin moving the ball!\n2. Use left and right arrow keys to move the paddle\n3. You get three lives at the beginning of each level\n4. Hit bricks with the ball to break them\n5. Bounce the ball on the left third to move it left,\n    middle to go straight up,\n    and right third to move the ball to the right";
-    private int rulesTextHeight = 120;
-    private String startingInstructions = "Click to begin!";
 
     /**
      * Constructor, creates a scene, a stage, and then set the stage to that scene
-     * @param startRoot the first board
      */
-    public Visualizer(Group startRoot) {
+    public Visualizer(){
         root = new Group();
+        grid = new GridPane();
+        grid.setHgap(GAP);
+        grid.setVgap(GAP);
         myScene = new Scene(root, CA_WIDTH, CA_HEIGHT, BACKGROUND);
         //super(startRoot, CA_WIDTH, CA_HEIGHT, BACKGROUND);
     }
@@ -44,24 +43,8 @@ public class Visualizer extends Application {
     @Override
     public void start(Stage primaryStage){
         // set intro text
-        int size = CA_WIDTH;
-        Text introText = new Text(intro);
-        introText.setTextAlignment(TextAlignment.CENTER);
-        introText.setStroke(Color.BLACK);
-        introText.setLayoutX(size/3);
-        introText.setLayoutY(size/3);
-        root.getChildren().add(introText);
-
-        // set rules text
-        Text rulesText = new Text(rules);
-        rulesText.setTextAlignment(TextAlignment.LEFT);
-        rulesText.setX(size/5);
-        rulesText.setY(size/3+introTextHeight);
-        root.getChildren().add(rulesText);
-
+        grid.getChildren().addAll(root);
         primaryStage.setScene(myScene);
-        primaryStage.setTitle(intro);
-
         primaryStage.show();
     }
 
@@ -74,12 +57,19 @@ public class Visualizer extends Application {
        // start(myStage);
     }
 
+    public void sendRoot(Group boardRoot){
+        root = boardRoot;
+    }
+
     public static void main(String[] args) {
 
-        //Group t = new Group();
-        //t.getChildren().add(new Rectangle(10,10,10,10));
-        //Visualizer v = new Visualizer(t);
-        //v.start(new Stage());
+        Group t = new Group();
+        Percolation perc = new Percolation();
+        Board b = new Board(20,20, perc);
+        t.getChildren().add(b.boardView());
+
+        Visualizer v = new Visualizer();
+        v.sendRoot(t);
 
         launch(args);
     }
