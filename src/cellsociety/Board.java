eb9ed66340c5ddc16bdca53faf1f02a0144675a7
Dana Mulligan <dana.mulligan@duke.edu>
2020-02-01 19:00:29 -0500

    refactored adding neighbors to a cell in Board to allow for corners to be added or not added based on rules

--- src/cellsociety/Board.java
+++ src/cellsociety/Board.java
@@ -73,38 +73,30 @@ public class Board implements Cloneable{
       for (int col = 0; col < myCols; col++) {
         Cell cell = cells[row][col];
         if (row + 1 < myRows) {
-          addNeighborCols(cell, col, cells, row+1);
+          addNeighborCols(cell, col, cells, row+1, myRules.areCornersNeighbors());
         }
         if (row > 0) {
-          addNeighborCols(cell, col, cells, row-1);
+          addNeighborCols(cell, col, cells, row-1, myRules.areCornersNeighbors());
         }
         if (col + 1 < myCols) {
-          addNeighborRows(cell, row, cells, col+1);
+          cell.addNeighbor(cells[row][col+1]);
         }
         if (col > 0) {
-          addNeighborRows(cell, row, cells, col-1);
+          cell.addNeighbor(cells[row][col-1]);
         }
       }
     }
   }
 
-  private void addNeighborCols(Cell cell, int col, Cell[][] cells, int i) {
-    cell.addNeighbor(cells[i][col]);
-    if (col > 0) {
-      cell.addNeighbor(cells[i][col-1]);
-    }
-    if (col + 1 < myCols) {
-      cell.addNeighbor(cells[i][col+1]);
-    }
-  }
-
-  private void addNeighborRows(Cell cell, int row, Cell[][] cells, int j) {
-    cell.addNeighbor(cells[row][j]);
-    if (row > 0) {
-      cell.addNeighbor(cells[row-1][j]);
-    }
-    if (row + 1 < myRows) {
-      cell.addNeighbor(cells[row+1][j]);
+  private void addNeighborCols(Cell cell, int col, Cell[][] cells, int neighborRow, boolean corners) {
+    cell.addNeighbor(cells[neighborRow][col]);
+    if(corners) {
+      if (col > 0) {
+        cell.addNeighbor(cells[neighborRow][col - 1]);
+      }
+      if (col + 1 < myCols) {
+        cell.addNeighbor(cells[neighborRow][col + 1]);
+      }
     }
   }
 
