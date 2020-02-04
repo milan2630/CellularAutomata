d444e8966420044146f8d688a687769638c2f1fa
Dana Mulligan <dana.mulligan@duke.edu>
2020-02-04 00:49:54 -0500

    removed blank spaces and commented Visualizer class

--- src/display/Visualizer.java
+++ src/display/Visualizer.java
@@ -1,31 +1,30 @@
 package display;
 
 import javafx.application.Application;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-
+/**
+ * creates a view of the CA simulation, extends application
+ */
 public class Visualizer extends Application {
 
-    public static final int CA_WIDTH = 500; //TODO THIS CAN BE CHANGED LATER
+    public static final int CA_WIDTH = 500;
     public static final int CA_HEIGHT = 500;
-    public static final Color BACKGROUND = Color.LAVENDERBLUSH; //TODO THIS CAN BE CHANGED LATER
+    public static final Color BACKGROUND = Color.LAVENDERBLUSH;
     public static final double GAP = 190;
 
     private Scene myScene;
     private GridPane grid;
     private Stage myStage;
 
-
     /**
      * Constructor, creates a scene, a stage, and then set the stage to that scene
      */
-    public Visualizer(){
+    public Visualizer() {
         grid = new GridPane();
         grid.setHgap(GAP);
         grid.setVgap(GAP);
@@ -38,17 +37,18 @@ public class Visualizer extends Application {
     @Override
     public void start(Stage primaryStage){
         myStage = primaryStage;
-        Rectangle2D screen = Screen.getPrimary().getVisualBounds();
         myStage.setX(0);
         primaryStage.setScene(myScene);
         primaryStage.show();
         primaryStage.setOnCloseRequest(t->stopEverything());
     }
 
+    /**
+     * Close the window
+     */
     public void closeWindow(){
         myStage.close();
     }
-
 
     /**
      *  Change the display
@@ -58,6 +58,7 @@ public class Visualizer extends Application {
         myScene = new Scene(grid, CA_WIDTH, CA_HEIGHT, BACKGROUND);
         start(stage);
     }
+
     private void stopEverything(){
         System.exit(1);
     }
