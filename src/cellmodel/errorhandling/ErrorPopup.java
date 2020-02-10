70803f2fe8fc171f3990623094e5f7a99592a105
Milan Bhat <mb554@duke.edu>
2020-02-10 02:33:55 -0500

    Commented error hadling files

--- src/cellmodel/errorhandling/ErrorPopup.java
+++ src/cellmodel/errorhandling/ErrorPopup.java
@@ -5,8 +5,15 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+/**
+ * Creates a popup saying what error occurred and keeps it up until it is closed
+ */
 public class ErrorPopup {
 
+    /**
+     * Creates a popup that simply has the message of the thrown Exception printed
+     * @param e is the exception that caused the error
+     */
     public ErrorPopup(Exception e){
         Stage errorStage = new Stage();
         errorStage.setTitle("Error");
