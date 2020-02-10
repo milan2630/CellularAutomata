bcc81fd3de3250bcecc7af039d900838e90b65f6
Dana Mulligan <dana.mulligan@duke.edu>
2020-02-09 21:17:29 -0500

    removed protected variables and added getters+setters

--- src/display/visualizer/TriangleVisualizer.java
+++ src/display/visualizer/TriangleVisualizer.java
@@ -2,63 +2,81 @@ package display.visualizer;
 
 public class TriangleVisualizer extends Visualizer {
 
-  private boolean pointyUp;
+  private boolean pointUp;
   /**
    * Constructor, creates a scene, a stage, and then set the stage to that scene
    */
   public TriangleVisualizer() {
     super();
-    pointyUp = false;
-    System.out.println("initializating pointUp to false");
+    pointUp = false;
   }
 
   private void setPointDirection() {
-    pointyUp = col%2 != row%2;
+    pointUp = getCol()%2 != getRow()%2;
   }
 
+  /**
+   * move to the next row, update y position depending on if the triangles touch at the tip or side,
+   * reset x position, reset column number, and update the direction the triangle points
+   */
   @Override
-  void moveToNextRow(){
-    row++;
-    xPos = 0;
-    if(row%2 == 1){
-      yPos=height+height+yPos;
+  protected void moveToNextRow(){
+    incrementRow();
+    resetXPos();
+    if(getRow()%2 == 1){
+      incrementYPos(getHeight()+getHeight());
     }
-    col = 0;
+    resetCol();
     setPointDirection();
   }
 
+  /**
+   * move to the next column
+   * set the x and y position to the "point" of the triangle by
+   * incrementing the x position to half the width of the triangle,
+   * and incrementing/decrementing the y position by the height of the triangle
+   * update point direction for the new triangle
+   */
   @Override
-  void moveOver(){
-    xPos += width/2;
-    if(pointyUp){
-      yPos -= height;
+  protected void moveOver(){
+    incrementXPos(getWidth()/2);
+    if(pointUp){
+      incrementYPos(-1*getHeight());
     } else {
-      yPos += height;
+      incrementYPos(getHeight());
     }
-    col++;
+    incrementCol();
     setPointDirection();
   }
 
+  /**
+   * make a Double array of all the points of the triangle
+   * @return that array
+   */
   @Override
-  Double[] getCorners(){
+  protected Double[] getCorners(){
     return new Double[]{
         //leftmost point
-        xPos, yPos,
+        getXPos(), getYPos(),
         //rightmost point
-        xPos + width, yPos,
+        getXPos() + getWidth(), getYPos(),
         //middle point
-        xPos + width / 2, yPos + height * getThirdTriangleYPoint(pointyUp)
+        getXPos() + getWidth() / 2, getYPos() + getHeight() * getThirdTriangleYPoint(pointUp)
     };
   }
 
+  /**
+   * reset all of the variables that we need to reset
+   */
   @Override
-  void resetVariables(){
-    xPos = 0;
-    col = 0;
-    row = 0;
-    pointyUp = false;
-    yPos = 0;
+  protected void resetVariables(){
+    resetXPos();
+    resetCol();
+    resetRow();
+    pointUp = false;
+    resetYPos();
   }
+
   private int getThirdTriangleYPoint(boolean up){
     if(up){
       return -1;
